6d490f42aba4de89ce841ef5252c22f745d4d728
donthosthate <donthosthate.to@protonmail.com>
2021-01-21 16:37:12 -0500

    backend-frontend communication now works with test data

--- background.js
+++ background.js
@@ -1,76 +1,10 @@
-var views = chrome.extension.getViews({
-    type: "popup"
-});
-let providerList = undefined;
-for (var i = 0; i < views.length; i++) {
-    var listMaybe = views[i].document.getElementById('providerList');
-    if (listMaybe != undefined) {
-      providerList = listMaybe;
-      break;
-    }
-}
-
-// spreadsheet location from url
-var SPREADSHEET_KEY = '1lklFq55OkWwbzsc0X52yqhlJYxRncwVhKFjnZ1-JPV8';
-// API key from the developer console
-var API_KEY = 'AIzaSyAa10uhcAU08bFvxpQiM8cArW6tlRH6YZQ';
-// this is an extremely aggressive range. :D
-var RANGE = 'A1:ZZ10000'
-var spreadsheet_url =  `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_KEY}/values/${RANGE}?key=${API_KEY}`
-var spreadsheet_data = [];
-
-/**
- * Get all the data from the spreadsheet!
- * https://docs.google.com/spreadsheets/d/1lklFq55OkWwbzsc0X52yqhlJYxRncwVhKFjnZ1-JPV8/edit
- */
-function getProvidersList() {
-  fetch(spreadsheet_url).then(function(response) {
-      return response.json();
-    }).then(function(result) {
-      spreadsheet_data = result.values;
-      displayProviders();
-    });
-}
-
-function displayProviders() {
-  // Ordering from the spreadsheet
-  var name_col = spreadsheet_data[0].indexOf('Name');
-  var url_col = spreadsheet_data[0].indexOf('URL');
-  var provides_col = spreadsheet_data[0].indexOf('Provides');
-  var tos_col = spreadsheet_data[0].indexOf('TOS');
-  var clause_col = spreadsheet_data[0].indexOf('Clause');
-  var contact_col = spreadsheet_data[0].indexOf('Contact');
-
-  // go through the remaining rows of the spreadsheet and parse 'em
-  var providers = spreadsheet_data.slice(1).map(function(provider, i) {
-    var ulNode = document.createElement("li");
-    var providerNode = `<b>${provider[name_col]}</b> (${provider[provides_col]}): <a href="${provider[tos_col]}#:~:text=${escape(provider[clause_col])}">Terms of Service</a> and <a href="${provider[contact_col]}">contact</a>`
-    // how to highlight text in Chrome: url...bla#:~:text=escaped%20text
-    ulNode.innerHTML = providerNode;
-
-    // only show it if it is germane to this particular website. for now, if random :p
-    if (Math.random() > .8)
-      return ulNode;
-    return undefined;
-  });
-
-  // strip out null providers before display
-  providers = providers.filter(function(provider) {
-    return provider != undefined;
-  });
-
-  for (var i = 0; i < providers.length; i++) {
-    providerList.appendChild(providers[i]);
-  }
-  console.log(providerList);
-}
-
-getProvidersList();
-
-
 /* Keep track of the active tab in each window */
 var activeTabs = {};
 
+/* Track the TLDs loaded by any of the active tabs */
+var activeTabsLoadedURLs = new Map();
+
+/* Listen for when we activate a new tab */
 chrome.tabs.onActivated.addListener(function(details) {
     activeTabs[details.windowId] = details.tabId;
 });
@@ -82,7 +16,6 @@ chrome.windows.onRemoved.addListener(function(winId) {
 
 /* Listen for web-requests and filter them */
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-//chrome.devtools.network.onRequestFinished.addListener(function(details) {
     if (details.tabId == -1) {
         console.log("Skipping request from non-tabbed context...");
         return;
@@ -91,7 +24,15 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     var notInteresting = Object.keys(activeTabs).every(function(key) {
         if (activeTabs[key] == details.tabId) {
             /* We are interested in this request */
-            console.log("Check this out:", details);
+            console.log("Check this out: ", details);
+            hostname_requestor = new URL(details.initiator).hostname;
+            hostname_requested = new URL(details.url).hostname;
+            if (activeTabsLoadedURLs.has(hostname_requestor)) {
+              activeTabsLoadedURLs.get(hostname_requestor).push(hostname_requested);
+            }
+            else {
+              activeTabsLoadedURLs.set(hostname_requestor, [hostname_requested]);
+            }
             return false;
         } else {
             return true;
@@ -100,7 +41,7 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
     if (notInteresting) {
         /* We are not interested in this request */
-        console.log("Just ignore this one:", details);
+        //console.log("Just ignore this one:", details);
     }
 }, { urls: ["<all_urls>"] });
 
@@ -112,7 +53,37 @@ chrome.tabs.query({ active: true }, function(tabs) {
     console.log("activeTabs = ", activeTabs);
 });
 
-console.log("MUTHAFUCKAAAAAA");
+//for sending a message
+//chrome.runtime.sendMessage({tlds: ["shopify.com","wp.com"]}, function(response) {});
+
+
+function messageReceived(msg) {
+   console.log("message received from popup!", msg);
+   // tabOfInterest: tablink
+   // we want to send back all the websites we have found that were loaded by this link's TLD
+   // for now, we send a test message...
+   tldsOfInterest = {"tlds": ["shopify.com","wp.com"]};
+   chrome.runtime.sendMessage(tldsOfInterest, function(response) {return true;});
+   console.log("message sent to popup!", tldsOfInterest);
+   return true;
+}
+
+chrome.extension.onConnect.addListener(function(port) {
+      console.log("Connected to popup");
+      port.onMessage.addListener(function(msg) {
+        console.log("message received from popup!", msg);
+        // tabOfInterest: tablink
+        // we want to send back all the websites we have found that were loaded by this link's TLD
+        // for now, we send a test message...
+        tldsOfInterest = {"tlds": ["shopify.com","wp.com"]};
+        port.postMessage(tldsOfInterest);
+        console.log("message sent to popup!", tldsOfInterest);
+      });
+ })
+
+
+//for listening any message which comes from runtime
+chrome.runtime.onMessage.addListener(messageReceived);
 
 // chrome.devtools.network.getHAR(function(result) {
 //   var entries = result.entries;
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let providerList = document.getElementById('providerList');
+let providers = new Map();
 
 // spreadsheet location from url
 var SPREADSHEET_KEY = '1lklFq55OkWwbzsc0X52yqhlJYxRncwVhKFjnZ1-JPV8';
@@ -18,11 +19,12 @@ function getProvidersList() {
       return response.json();
     }).then(function(result) {
       spreadsheet_data = result.values;
-      displayProviders();
+      sortProviders();
     });
 }
 
-function displayProviders() {
+// once we get all the spreadsheet data, turn it into some nice little display items
+function sortProviders() {
   // Ordering from the spreadsheet
   var name_col = spreadsheet_data[0].indexOf('Name');
   var url_col = spreadsheet_data[0].indexOf('URL');
@@ -32,27 +34,46 @@ function displayProviders() {
   var contact_col = spreadsheet_data[0].indexOf('Contact');
 
   // go through the remaining rows of the spreadsheet and parse 'em
-  var providers = spreadsheet_data.slice(1).map(function(provider, i) {
+  spreadsheet_data.slice(1).map(function(provider, i) {
     var ulNode = document.createElement("li");
     var providerNode = `<b>${provider[name_col]}</b> (${provider[provides_col]}): <a href="${provider[tos_col]}#:~:text=${escape(provider[clause_col])}">Terms of Service</a> and <a href="${provider[contact_col]}">contact</a>`
     // how to highlight text in Chrome: url...bla#:~:text=escaped%20text
     ulNode.innerHTML = providerNode;
 
-    // only show it if it is germane to this particular website. for now, if random :p
-    if (Math.random() > .8)
-      return ulNode;
-    return undefined;
+    providers.set(provider[url_col], ulNode);
   });
 
-  // strip out null providers before display
-  providers = providers.filter(function(provider) {
-    return provider != undefined;
+  // when we actually load the popup, we need to let our background tab know which url we want deets for
+  // note that we moved this into the sort function... we can't do it before we have the other info
+  chrome.tabs.getSelected(null,function(tab) {
+      var tablink = tab.url;
+      //for sending a message
+      port.postMessage({tabOfInterest: tablink});
   });
-
-  for (var i = 0; i < providers.length; i++) {
-    providerList.appendChild(providers[i]);
-  }
-  console.log(providerList);
 }
+
+// take some tlds and actually display them in the thing
+function displayProviders(tlds) {
+  console.log(providers);
+  for (var i = 0; i < tlds.length; i++) {
+    if (providers.has(tlds[i])) {
+      providerList.appendChild(providers.get(tlds[i]));
+    }
+  }
+}
+
+function messageReceived(msg) {
+  // tlds: ["shopify.com","wp.com"]
+  displayProviders(msg["tlds"]);
+  return true;
+}
+
+var port = chrome.extension.connect({
+  name: "TLD Transfer Pipe"
+});
+port.onMessage.addListener(messageReceived);
+
+//for listening any message which comes from runtime
+//chrome.runtime.onMessage.addListener(messageReceived);
 
 getProvidersList();
